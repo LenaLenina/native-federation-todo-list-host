77bf50e974e5ff7a352f073e32a02b8adfbd1c3a
DESKTOP-AG49DLA\pc_27_05_2023 <lenalenina5677@gmail.com>
2023-12-26 17:53:59 +0300

    adding module content

--- projects/host/src/app/app-routing.module.ts
+++ projects/host/src/app/app-routing.module.ts
@@ -1,10 +1,56 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { loadRemoteModule } from '@angular-architects/native-federation';
+import { ConfigService } from './services/config.service';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+];
+
+export function initializeRoutes(router: Router, configService: ConfigService) {
+  return (): Observable<any> => {
+    return configService.getConfig()
+    .pipe(
+      switchMap((routesJson) => {
+        if (!Array.isArray(routesJson)) {
+          console.error('routesJson is not an array');
+          return [];
+        }
+
+        let dynamicRoutes = routesJson.map((route) => ({
+          path: route.path,
+          loadChildren: () =>
+            loadRemoteModule({
+              remoteName: route.remoteName,
+              exposedModule: route.exposedModule,
+            })
+              .then((m) => m[route.module])
+              .catch((err) => console.log(err)),
+        }));
+
+        router.resetConfig([...routes, ...dynamicRoutes]);
+        return [];
+      })
+    );
+  };
+}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeRoutes,
+      multi: true,
+      deps: [Router, ConfigService]
+    }
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor() { }
+}
--- projects/host/src/app/app.module.ts
+++ projects/host/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
